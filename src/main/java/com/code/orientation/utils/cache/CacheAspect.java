26fa05f15b5815089b2fb7cb71e0ca4732fc380b
xdznHexin <2387740264@qq.com>
2024-04-11 22:07:59 +0800

    fix：优化代码格式

--- src/main/java/com/code/orientation/utils/cache/CacheAspect.java
+++ src/main/java/com/code/orientation/utils/cache/CacheAspect.java
@@ -20,103 +20,118 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 缓存切面
+ *
+ * @author HeXin
+ * @date 2024/04/11
+ */
 @Aspect
 @Component
 @Slf4j
 public class CacheAspect {
-    @Autowired
-    private RedisUtil redisUtil;
+    private final RedisUtil redisUtil;
+
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    public CacheAspect(RedisUtil redisUtil, ObjectMapper objectMapper) {
+        this.redisUtil = redisUtil;
+        this.objectMapper = objectMapper;
+    }
 
     @Pointcut("@annotation(com.code.orientation.utils.cache.Cache)")
     public void pointcut(){
     }
 
     @Around("pointcut()")
-    public Object around(ProceedingJoinPoint joinPoint){
+    public Object around(ProceedingJoinPoint joinPoint) {
+        Method realMethod = getRealMethod(joinPoint);
+        Cache cache = AnnotationUtils.findAnnotation(realMethod, Cache.class);
+        String redisKey = generateRedisKey(cache, joinPoint);
+        String json = redisUtil.getString(redisKey);
+
+        if (StringUtils.isNotBlank(json)) {
+            log.info("命中缓存 <== key：{}", redisKey);
+            return readValue(json, realMethod.getReturnType());
+        }
+
+        if (json != null) {
+            log.info("命中空数据 <== key：{}", redisKey);
+            throw new CustomException(CodeEnum.DATA_NOT_EXIST);
+        }
+
+        try {
+            Object result = joinPoint.proceed();
+            if (result == null) {
+                throw new CustomException(CodeEnum.DATA_NOT_EXIST);
+            }
+            log.info("缓存数据 ==> key：{}", redisKey);
+            setCache(redisKey, Objects.requireNonNull(cache), result);
+            return result;
+        } catch (Throwable e) {
+            handleException(redisKey, e);
+            throw new CustomException(CodeEnum.DATA_NOT_EXIST);
+        }
+    }
+
+    private Method getRealMethod(ProceedingJoinPoint joinPoint) {
         Signature signature = joinPoint.getSignature();
         Method method = ((MethodSignature) signature).getMethod();
-        Method realMethod;
         try {
-            realMethod = joinPoint.getTarget().getClass()
-                    .getDeclaredMethod(signature.getName(),method.getParameterTypes());
+            return joinPoint.getTarget().getClass()
+                    .getDeclaredMethod(signature.getName(), method.getParameterTypes());
         } catch (NoSuchMethodException e) {
-            log.error("反射错误");
-            e.printStackTrace();
+            log.error("反射错误", e);
             throw new CustomException(e);
         }
-        Class<?> returnType = method.getReturnType();
-        // 获取注解
-        Cache cache = AnnotationUtils.findAnnotation(realMethod, Cache.class);
-        String key = cache.key();
-        long time = cache.time();
-        TimeUnit timeUnit = cache.timeUnit();
-        RedisConstants constants = cache.constants();
-        boolean isDefault = constants == RedisConstants.DEFAULT;
-        // 获取参数
+    }
+
+    private String generateRedisKey(Cache cache, ProceedingJoinPoint joinPoint) {
         Object[] args = joinPoint.getArgs();
         StringBuilder params = new StringBuilder();
         for (Object arg : args) {
-            if(arg != null){
-                String value = null;
+            if (arg != null) {
                 try {
-                    objectMapper.writeValueAsString(arg);
+                    params.append(objectMapper.writeValueAsString(arg));
                 } catch (JsonProcessingException e) {
                     throw new CustomException(e);
                 }
-                params.append(value);
             }
         }
-        if(StringUtils.isNotBlank(params.toString())){
-            // 参数加密：防止出现key过长以及字符转义获取不到的情况
+        if (StringUtils.isNotBlank(params.toString())) {
             params = new StringBuilder(MD5.create().digestHex(params.toString()));
         }
-        String redisKey;
-        if(isDefault){
-            redisKey = key + ":"+ params;
-        }else {
-            redisKey = constants.getKey() + key + ":" + params;
-        }
-        String json = redisUtil.getString(redisKey);
-        if(StringUtils.isNotBlank(json)){
-            log.info("命中缓存 <== key：{}",redisKey);
-            try {
-                return objectMapper.readValue(json,returnType);
-            } catch (JsonProcessingException e) {
-                throw new CustomException(e);
-            }
-        }
-        if(json != null){
-            // 空数据
-            log.info("命中空数据 <== key：{}",redisKey);
-            throw new CustomException(CodeEnum.DATA_NOT_EXIST);
-        }
-        // 缓存未命中，将数据存入缓存
-        Object result;
-        try {
-            result = joinPoint.proceed();
-        } catch (Throwable e) {
-            e.printStackTrace();
-            // 防止缓存穿透
-            log.info("缓存空数据 ==> key：{}",redisKey);
-            redisUtil.set(redisKey," ",10L,TimeUnit.SECONDS);
-            throw new CustomException(CodeEnum.DATA_NOT_EXIST);
-        }
-        if(result == null){
-            // 缓存空数据
-            redisUtil.set(redisKey," ",10L,TimeUnit.SECONDS);
-            throw new CustomException(CodeEnum.DATA_NOT_EXIST);
-        }
-        log.info("缓存数据 ==> key：{}",redisKey);
-        if(isDefault){
-            redisUtil.set(redisKey,result,time,timeUnit);
-        } else {
-            redisUtil.set(constants,redisKey,result);
-        }
-        return result;
+        String key = cache.key();
+        RedisConstants constants = cache.constants();
+        boolean isDefault = constants == RedisConstants.DEFAULT;
+        return isDefault ? key + ":" + params : constants.getKey() + key + ":" + params;
     }
+
+    private Object readValue(String json, Class<?> returnType) {
+        try {
+            return objectMapper.readValue(json, returnType);
+        } catch (JsonProcessingException e) {
+            throw new CustomException(e);
+        }
+    }
+
+    private void setCache(String redisKey, Cache cache, Object result) {
+        RedisConstants constants = cache.constants();
+        if (cache.constants() == RedisConstants.DEFAULT) {
+            redisUtil.set(redisKey, result, cache.time(), cache.timeUnit());
+        } else {
+            redisUtil.set(constants, redisKey, result);
+        }
+    }
+
+    private void handleException(String redisKey, Throwable e) {
+        log.error("错误信息：{}",e.getMessage());
+        log.info("缓存空数据 ==> key：{}", redisKey);
+        redisUtil.set(redisKey, " ", 10L, TimeUnit.SECONDS);
+    }
+
 }
